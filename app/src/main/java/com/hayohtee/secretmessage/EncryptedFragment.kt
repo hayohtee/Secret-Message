374e9c0980da2c75362330e7e39e5348d6adfb2a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-01-10 18:46:12 +0100

    Update EncryptedFragment.kt
    Use a Safe Args to get the require arguments and then get a reference to the encryptedTextView then set its text to the message argument in reversed

--- app/src/main/java/com/hayohtee/secretmessage/EncryptedFragment.kt
+++ app/src/main/java/com/hayohtee/secretmessage/EncryptedFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class EncryptedFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +13,13 @@ class EncryptedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_encrypted, container, false)
+        val view = inflater.inflate(R.layout.fragment_encrypted, container, false)
+
+        val message = EncryptedFragmentArgs.fromBundle(requireArguments()).message
+
+        val encryptedTextView = view.findViewById<TextView>(R.id.encrypted_textView)
+        encryptedTextView.text = message.reversed()     // encrypt the message by reversing it
+
+        return view
     }
 }
