a255f87f77cd2971288d4654300c1a79d1fecfb6
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-01-10 18:40:52 +0100

    Update MessageFragment.kt
    Get a reference to an EditText and next Button, then added an onClickListener to the nextButton. Use Safe Args to pass an argument to EncryptedFragment by generating Directions class then pass the direction to the NavController

--- app/src/main/java/com/hayohtee/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hayohtee/secretmessage/MessageFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +15,20 @@ class MessageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+
+        val editText = view.findViewById<EditText>(R.id.message_editText)
+
+        val nextButton = view.findViewById<Button>(R.id.next_button)
+        nextButton.setOnClickListener {
+            val message: String = editText.text.toString()
+            val direction = MessageFragmentDirections
+                .actionMessageFragmentToEncryptedFragment(message)
+
+            view.findNavController().navigate(direction)
+        }
+
+        return view
     }
 
 }
